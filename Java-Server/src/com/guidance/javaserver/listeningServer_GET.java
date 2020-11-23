d38fdc3d08eb1abefa2d6891eecdc2ea580edebb
Denis-Atanasov <deeennka@gmail.com>
2020-11-23 21:29:32 +0200

    adding some changes

--- Java-Server/src/com/guidance/javaserver/listeningServer_GET.java
+++ Java-Server/src/com/guidance/javaserver/listeningServer_GET.java
@@ -2,14 +2,17 @@ package com.guidance.javaserver;
 
 import java.net.*;
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class tcpServer {
+public class listeningServer_GETreqs {
     public static void main(String args[]){
         int port;
         ServerSocket socket;
         BufferedReader input;
         String pckgSent;
         PrintWriter output;
+        Matcher matcher;
 
         try {
             port = Integer.parseInt(args[0]);
@@ -26,22 +29,26 @@ public class tcpServer {
             //looping...
             while(true){
                 Socket sckt = socket.accept();
-                System.out.println(sckt.isClosed());
+                System.out.println(sckt.isClosed() + "1");
                 System.out.println("New connection accepted "
                         + sckt.getInetAddress()
                         + ":"
                         +sckt.getPort());
                 input = new BufferedReader(new InputStreamReader(sckt.getInputStream()));
                 System.out.println("Keep-Alive: "+ sckt.getKeepAlive());
-                System.out.println(sckt.isClosed());
+                System.out.println(sckt.isClosed() + "2");
                 //print received data
                 try {
-                    System.out.println(sckt.isClosed());
+                    //"\\^GET \\/users\\?ID\\=[1-9]{1,7}[\\s\\t\\n\\r]+HTTP\\/1\\.1$"
+                    System.out.println(sckt.isClosed() + "3");
                     while(true){
                         String msg = input.readLine();
                         if (msg==null) break;
                         System.out.println(msg);
-                        if (msg.equals("PING|")){
+                        if (msg.matches
+                                ("\\^GET \\/users\\?ID\\=[1-9]{1,7}[\\s\\t\\n\\r]+HTTP\\/1\\.1[\\s\\t\\n\\r]*$")){
+                            System.out.println(msg.substring(
+                                    msg.indexOf("=")+1, msg.indexOf("H")-1));
                             System.out.println("Flag for alive is set to: " +sckt.getKeepAlive());
                             output = new PrintWriter(sckt.getOutputStream(),true);
                             //get user input and transmit it to server
@@ -49,9 +56,11 @@ public class tcpServer {
                             //stop if input line is "."
                             output.println("Data sent: "+ pckgSent);
                             sckt.close();
+                        }else if(msg.equals("POST / HTTP/1.1")){
+                            System.out.println("kure, POST e");
                         }
                     }
-                    System.out.println(sckt.isClosed());
+                    System.out.println(sckt.isClosed() + "4");
                 }
                 catch (IOException e){
                     System.out.println(e);
@@ -64,3 +73,4 @@ public class tcpServer {
         }
     }
 }
+
---
+++ Java-Server/src/com/guidance/javaserver/listeningServer_POST.java
@@ -0,0 +1,62 @@
+package com.guidance.javaserver;
+
+import java.net.*;
+import java.io.*;
+
+public class listeningServer_POST {
+    public static void main(String[] args) {
+        int port;
+        ServerSocket servSocket;
+        Socket clientSocket;
+        BufferedReader input;
+        String pckgSent;
+        PrintWriter output;
+
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+            System.out.println("port = 1500(default)");
+            port = 16001;
+        }
+
+        try {
+            servSocket = new ServerSocket(port);
+            System.out.println("Server waiting for client on port " + servSocket.getLocalPort() + "\n");
+
+            //looping...
+            while (true) {
+                clientSocket = servSocket.accept();
+                System.out.println("New connection accepted "
+                        + clientSocket.getInetAddress()
+                        + ":"
+                        + clientSocket.getPort());
+                input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("Keep-Alive: " + clientSocket.getKeepAlive());
+                //print received data
+                try {
+                    //"\\^GET \\/users\\?ID\\=[1-9]{1,7}[\\s\\t\\n\\r]+HTTP\\/1\\.1$"
+                    while (true) {
+                        String msg = input.readLine();
+                        if (msg == null) break;
+                        System.out.println(msg);
+                        if (msg.contains("GET")) {
+                            System.out.println(msg.substring(
+                                    msg.indexOf("=") + 1, msg.indexOf("H") - 1));
+                        }
+                        output = new PrintWriter(clientSocket.getOutputStream(), true);
+                        //get user input and transmit it to server
+                        pckgSent = "OK"; // here for returning ?
+                        //stop if input line is "."
+                        output.println("Data sent: " + pckgSent);
+                        clientSocket.close();
+
+                    }
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+                //connection close by client
+            }
+        }
+        catch(IOException e){System.out.println(e);}
+    }
+}

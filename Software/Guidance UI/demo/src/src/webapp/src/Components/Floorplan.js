e507cd9b41361d3bc5178bf63c65e1698bdc59b9
DylanArts <42747113+DylanArts@users.noreply.github.com>
2020-12-12 22:30:29 +0100

    mainpage zooming update
    Nodes and other elements on the floorplan will now be zoomed correctly with the floorplan

--- Software/Guidance UI/demo/src/src/webapp/src/Components/Floorplan.js
+++ Software/Guidance UI/demo/src/src/webapp/src/Components/Floorplan.js
@@ -38,7 +38,7 @@ class Floorplan extends Component {
     }
 
     _scroll(e) {
-        const floorplan = document.getElementById("floorplan-container-image");
+        const floorplan = document.getElementById("floorplan-container");
 
         if(e.deltaY > 0 && this.state.scroll < 4.5) { //Max scale 450%
             floorplan.style.transform = "scale("+ (this.state.scroll += 0.1) +")"; //Scale up 10%
@@ -50,7 +50,8 @@ class Floorplan extends Component {
 
     render() {
         return (
-            <div>
+            <div id={"floorplan-container"}>
+                <div style={{position: "absolute", left: "1070px", top: "320px", backgroundColor: "#2166cf", padding: "8px", borderRadius: "45px", border: "1px solid white"}}/>
                 <img alt=""
                      className="img"
                      id="floorplan-container-image"
